045f288c1fda6614a8bf2b5c022ef5ea40d797d2
Stephen Cagle <stephen@read-line.com>
2016-12-22 16:37:41 -0800

    Put the test at the end

--- src/hackerrank/challenges/bigger_is_greater.clj
+++ src/hackerrank/challenges/bigger_is_greater.clj
@@ -3,7 +3,6 @@
    [clojure.spec :as s]
    [clojure.spec.gen :as gen]
    [clojure.spec.test :as stest]
-   [clojure.test.check.generators :as cgen]
    [clojure.string :refer [join]]
    [clojure.test :as test :refer [deftest testing is]]))
 
@@ -31,6 +30,22 @@
   (when-let [r (-> s vec next-permutation)]
     (join "" r)))
 
+#_(doseq [i (range (Integer/parseInt (read-line)))
+          :let [s (read-line)]]
+    (-> s bigger (or "no answer") println))
+
+(deftest bigger-is-greater-test []
+  (is (= nil (bigger "a")))
+  (is (= "ba" (bigger "ab")))
+  (is (= nil (bigger "bb")))
+  (is (= "hegf" (bigger "hefg")))
+  (is (= "dhkc" (bigger "dhck")))
+  (is (= "hcdk" (bigger "dkhc")))
+  (is (= "baaa" (bigger "abaa")))
+  (is (= nil (bigger "321")))
+  (is (= "987123456" (bigger "986754321")))
+  )
+
 (s/def ::pos-int (s/and int? #(> % 0)))
 (s/def ::pos-int-str (gen/fmap #(str (if ((complement pos?) %) (-> % (* -1) inc) %)) (s/gen int?)))
 (s/def ::pos-int-str (s/with-gen (s/and string? #(-> % count pos?))
@@ -57,19 +72,3 @@
              (every?
               #(not= expected %)
               (map #(-> % str frequencies) between))))))
-
-(deftest bigger-is-greater-test []
-  (is (= nil (bigger "a")))
-  (is (= "ba" (bigger "ab")))
-  (is (= nil (bigger "bb")))
-  (is (= "hegf" (bigger "hefg")))
-  (is (= "dhkc" (bigger "dhck")))
-  (is (= "hcdk" (bigger "dkhc")))
-  (is (= "baaa" (bigger "abaa")))
-  (is (= nil (bigger "321")))
-  (is (= "987123456" (bigger "986754321")))
-  )
-
-#_(doseq [i (range (Integer/parseInt (read-line)))
-          :let [s (read-line)]]
-    (-> s bigger println))
